1dc1106a6bc2abcdb1148c11300c65d365600121
Tomasz Krynicki <tomasz.krynicki@eastsoft.pl>
2024-05-29 08:27:09 +0200

    alias in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig,searchForWorkspaceRoot  } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
-    });
+	plugins: [sveltekit()],
+	resolve:{
+		alias:{
+			components:'src/components'
+		} 
+	}
+    }
+	);
 
